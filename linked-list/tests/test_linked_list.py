c12c4ca714c609428c30bf7784f4e2b33a49fe92
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-03 17:16:08 -0700

    Re-submission-Passed all test plus new test added

--- linked-list/tests/test_linked_list.py
+++ linked-list/tests/test_linked_list.py
@@ -1,6 +1,6 @@
 import pytest
 
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_exists():
@@ -25,14 +25,14 @@ def test_populated_head():
     assert linked.head.value == "apple"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_empty():
     linked_list = LinkedList()
 
     assert str(linked_list) == "NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_single():
     linked_list = LinkedList()
 
@@ -41,7 +41,7 @@ def test_to_string_single():
     assert str(linked_list) == "{ apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_double():
     linked_list = LinkedList()
 
@@ -74,3 +74,31 @@ def test_includes_false():
     linked_list.insert("banana")
 
     assert not linked_list.includes("cucumber")
+
+
+def test_create_node():
+    node1 = Node(10)
+    assert node1.value == 10
+
+
+def test_create_node_not_pass():
+    node1 = Node(10)
+    assert node1.value != 20
+
+
+def test_create_node_test_next():
+    node1 = Node(50)
+    assert node1.next_node is None
+
+
+def test_new_ll():
+    ll = LinkedList()
+    assert ll.head is None
+
+
+def test_new_ll_with_node():
+    node1 = Node(1000)
+    node2 = Node(990, node1)
+    ll = LinkedList(node2)
+    assert ll.head is node2
+    assert ll.head.next_node is node1
