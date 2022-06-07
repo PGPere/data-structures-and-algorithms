c8d33d70912e45e174e7772e66cb7216a4333e95
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-06 22:54:36 -0700

    updated6

--- tests/test_linked_list_insertions.py
+++ tests/test_linked_list_insertions.py
@@ -1,18 +1,18 @@
 import pytest
-
-from linked_list_insertion import LinkedList
-
-
+ 
+from linked_list_insertions.linked_list_insertion import LinkedList
+ 
+ 
 def test_exists():
-    assert LinkedList
-
-
+   assert LinkedList
+ 
+ 
 # @pytest.mark.skip("TODO")
 def test_instantiate():
-    assert LinkedList()
-
-
+   assert LinkedList()
+ 
+ 
 # @pytest.mark.skip("TODO")
 def test_empty_head():
-    linked = LinkedList()
-    assert linked.head is None
+   linked = LinkedList()
+   assert linked.head is None
