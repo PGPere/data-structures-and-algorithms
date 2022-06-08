52187bc1b16422ec01c428c490cd2fef3137127a
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-07 17:57:06 -0700

    insertions_fixed

---
+++ linked-list-insertions/tests/test_linked_list_insertions.py
@@ -0,0 +1,18 @@
+import pytest
+ 
+from linked_list_insertions.linked_list_insertion import LinkedList
+ 
+ 
+def test_exists():
+   assert LinkedList
+ 
+ 
+# @pytest.mark.skip("TODO")
+def test_instantiate():
+   assert LinkedList()
+ 
+ 
+# @pytest.mark.skip("TODO")
+def test_empty_head():
+   linked = LinkedList()
+   assert linked.head is None
