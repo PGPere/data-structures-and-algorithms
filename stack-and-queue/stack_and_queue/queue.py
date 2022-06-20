ed5d13c8a2110450d7fd2efcf00a6a14514854f7
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-20 12:18:00 -0700

    Passed all 21 tests

--- stack-and-queue/stack_and_queue/queue.py
+++ stack-and-queue/stack_and_queue/queue.py
@@ -1,22 +1,60 @@
+from stack_and_queue.node import Node
+from stack_and_queue.invalid_operation_error import InvalidOperationError
+from stack_and_queue.linked_list import LinkedList
+
+
 class Queue:
     """
     The quque class
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, rear=None, front=None):
+        self.rear = rear
+        self.front = front
 
-    def enqueue(self):
+    def enqueue(self, value):
         # enqueue
-        #   Arguments: value
+        # Arguments: value
         # adds a new node with that value to the back of the queue with an O(1) Time performance.
-        pass
+
+        node = Node(value)
+
+        front = self.front
+        rear = self.rear
+
+        if front is None:
+            self.front = node
+            self.rear = node
+            return
+
+        rear.next_node = node
+        self.rear = node
 
     def dequeue(self):
-        pass
+
+        if self.front is None:
+            raise InvalidOperationError
+
+        front = self.front
+
+        node_temp = self.front
+
+        self.front = self.front.next_node
+
+        node_temp.next_node = None
+
+        return node_temp.value
 
     def peek(self):
-        pass
+
+        if self.front is None:
+            raise InvalidOperationError
+
+        return self.front.value
 
     def is_empty(self):
-        pass
+
+        if not self.front:
+            return True
+
+        return False
