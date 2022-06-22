12eebd4206510f430a800063581e16064f42260d
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-21 21:58:38 -0700

    Passed all four tests

--- stack-queue-pseudo/stack_queue_pseudo/stack.py
+++ stack-queue-pseudo/stack_queue_pseudo/stack.py
@@ -7,48 +7,63 @@ class Stack:
     Stack Class
     """
 
-    def __init__(self, top=None):
-        self.top = top
+    # def __init__(self, top=None):
+    #     self.top = top
 
-    def push(self, value):
-        # INPUT <-- value to add, wrapped in Node internally
-        # OUTPUT <-- none
-        # Top = self.top
-        node = Node(value)
+    # def push(self, value):
+    #     # INPUT <-- value to add, wrapped in Node internally
+    #     # OUTPUT <-- none
+    #     # Top = self.top
+    #     node = Node(value)
 
-        if self.top is None:
-            self.top = node
+    #     if self.top is None:
+    #         self.top = node
 
-        node.next_node = self.top
-        self.top = node
+    #     node.next_node = self.top
+    #     self.top = node
+
+    # def pop(self):
+    #     # INPUT <-- No input
+    #     # OUTPUT <-- value of top Node in stack
+    #     # EXCEPTION if stack is empty
+
+    #     if self.top is None:
+    #         raise InvalidOperationError
+
+    #     node_temp = self.top
+
+    #     if self.top.next_node is None:
+    #         self.top = None
+    #     elif self.top.next_node is not None:
+    #         self.top = self.top.next_node
+
+    #     return node_temp.value
+
+    # def peek(self):
+
+    #     if self.top is None:
+    #         raise InvalidOperationError
+
+    #     return self.top.value
+
+    # def is_empty(self):
+
+    #     if self.top:
+    #         return True
+
+    #     return False
+
+    def __init__(self):
+        self.elements = []
+
+    def push(self, item):
+        self.elements.append(item)
 
     def pop(self):
-        # INPUT <-- No input
-        # OUTPUT <-- value of top Node in stack
-        # EXCEPTION if stack is empty
+        return self.elements.pop()
 
-        if self.top is None:
-            raise InvalidOperationError
-
-        node_temp = self.top
-
-        if self.top.next_node is None:
-            self.top = None
-        elif self.top.next_node is not None:
-            self.top = self.top.next_node
-
-        return node_temp.value
-
-    def peek(self):
-
-        if self.top is None:
-            raise InvalidOperationError
-
-        return self.top.value
+    def size(self):
+        return len(self.elements)
 
     def is_empty(self):
-
-        if self.top:
-            return True
-
-        return False
+        return self.size() == 0
