7477c121c7bc1d005cd5ecc3565eecc56745602b
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-21 22:08:53 -0700

    Passed all tests_refactored

--- stack-queue-pseudo/stack_queue_pseudo/stack_queue_pseudo.py
+++ stack-queue-pseudo/stack_queue_pseudo/stack_queue_pseudo.py
@@ -1,5 +1,4 @@
 from stack_queue_pseudo.stack import Stack
-from stack_queue_pseudo.node import Node
 
 
 class PseudoQueue():
@@ -9,13 +8,19 @@ class PseudoQueue():
         self.s2 = Stack()
 
     def enqueue(self, item):
+
         self.s1.push(item)
 
     def dequeue(self):
+
         if not self.s1.is_empty():
+
             while self.s1.size() > 0:
                 self.s2.push(self.s1.pop())
+
             pop_item = self.s2.pop()
+
             while self.s2.size() > 0:
                 self.s1.push(self.s2.pop())
+
             return pop_item
