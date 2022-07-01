e300e0b33d398cc81f812cbc4c93d4c9c1041973
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-30 21:45:00 -0700

    Passed 8 tests

--- stack-queue-brackets/stack_queue_brackets/stack_queue_brackets.py
+++ stack-queue-brackets/stack_queue_brackets/stack_queue_brackets.py
@@ -1,5 +1,19 @@
 from stack_queue_brackets.queue import Queue
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    open_brackets = tuple('{([')
+    close_brackets = tuple('})]')
+    map = dict(zip(open_brackets, close_brackets))
+    queue = Queue()
+
+    for i in string:
+        if i in open_brackets:
+            queue.enqueue(map[i])
+        elif i in close_brackets:
+            if not queue or i != queue.dequeue():
+                return False
+    if queue.is_empty:
+        return True
+    else:
+        return False
