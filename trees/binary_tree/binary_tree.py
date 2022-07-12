69e4e85ff1b99b48c569703d4ec9382067939c88
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-07-11 18:14:36 -0700

    Passed all 13 tests

--- trees/binary_tree/binary_tree.py
+++ trees/binary_tree/binary_tree.py
@@ -1,16 +1,75 @@
+from multiprocessing.sharedctypes import Value
+
+
 class BinaryTree:
-    """
-    Put docstring here
-    """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            values.append(root.value)
+
+            traverse(root.left)
+
+            traverse(root.right)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
+
+    def in_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            traverse(root.left)
+
+            values.append(root.value)
+
+            traverse(root.right)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
+
+    def post_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            traverse(root.left)
+
+            traverse(root.right)
+
+            values.append(root.value)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
 
 
 class Node:
-    pass
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"Node{self.value}"
