44107521f7a0cdcfe8371528a25248b4a17c2a48
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-08-30 00:35:44 -0400

    first commit

---
+++ tree-intersection/README.md
@@ -0,0 +1,16 @@
+
+# Tree Intersection
+
+## This is a binary tree intersection implementation
+
+# Challenge
+
+## Find common values in 2 binary trees
+
+# Whiteboard Process
+
+![alt text](https://github.com/PGPere/data-structures-and-algorithms/blob/5731ad1f6503823c2779b7d2d102bd6b573bf3d1/trees/Code%20Challenge%2016.png)
+
+## Approach & Efficiency
+
+### Binary tree has O(n) complexity for time & space.
---
+++ tree-intersection/tree_intersection/binary_tree.py
@@ -0,0 +1,93 @@
+from multiprocessing.sharedctypes import Value
+
+class BinaryTree:
+
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            values.append(root.value)
+
+            traverse(root.left)
+
+            traverse(root.right)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
+
+    def in_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            traverse(root.left)
+
+            values.append(root.value)
+
+            traverse(root.right)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
+
+    def post_order(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            traverse(root.left)
+
+            traverse(root.right)
+
+            values.append(root.value)
+
+        values = []
+
+        traverse(self.root)
+
+        return values
+
+    def find_maximum_value(self):
+
+        def traverse(root):
+
+            if not root:
+                return
+
+            values.append(root.value)
+
+            traverse(root.left)
+
+            traverse(root.right)
+
+        values = []
+
+        traverse(self.root)
+
+        return max(values)
+
+
+class Node:
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"Node{self.value}"
