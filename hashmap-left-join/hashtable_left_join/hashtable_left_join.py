37f7f13bd19f46558577dbc0341f3ed665861759
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-09-01 23:04:02 -0400

    code completed

--- hashmap-left-join/hashtable_left_join/hashtable_left_join.py
+++ hashmap-left-join/hashtable_left_join/hashtable_left_join.py
@@ -1,5 +1,14 @@
 from hashtable_left_join.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(hashtable1, hashtable2):
+
+    collection = []
+
+    for key in hashtable1.keys():
+
+        words = []
+        words.extend([key, hashtable1.get(key), hashtable2.get(key)])
+        collection.append(words)
+
+    return collection
